29742d6d9a39102faa173fb4fb1d200b2ff9e70e
Alexey 1981 <maximenko.av@rambler.ru>
2023-02-25 22:06:33 +0300

    Initial commit

--- src/bonus.java
+++ src/bonus.java
@@ -1,5 +1,18 @@
 public class bonus {
     public static void main(String[] args) {
-        System.out.println();
+        int a = 100;
+        int b = 13500;
+        int c;
+        int schet;
+        if (b > 1000) {
+            c = b / 100;
+            schet = a + b + c;
+        } else {
+            c = 0;
+            schet = a + b + c;
+        }
+//        int schet = a + b + c;
+        System.out.println("Сумма бонусов равна " + c);
+        System.out.println("Сумма счета равна " + schet);
     }
 }
